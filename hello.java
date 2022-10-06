e3827fad2d603238ab01a8ed59d23e20bffd93bc
Julia Fong <Juliamzfong@gmail.com>
2022-10-06 13:50:24 -0500

    initial commit

---
+++ hello.java
@@ -0,0 +1,11 @@
+/**
+ * hello
+ */
+public class hello {
+
+public static void main(String[] args) {
+    String x = "Test";
+    System.out.println(x);
+    }    
+}
+
---
+++ lesson1/lesson1.java
@@ -0,0 +1,27 @@
+/* 
+ * There are two ways to create our own Thread object
+ * 
+ * Subclassing the Thread class and instantiating a new object of that class
+ * Implementing the Runnable interface
+ * both cases the run()method should be implemented
+*/
+package lesson1;
+
+class MyClass extends Thread{
+    public void run() {
+        //TODO auto-generated method stub
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Value " + i);
+
+        }
+    }
+    
+}
+
+public class lesson1 {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.start();
+    }
+    
+}
