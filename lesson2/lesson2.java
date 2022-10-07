8100449ae53adce9796f6a363b6fe6778db53083
Julia Fong <Juliamzfong@gmail.com>
2022-10-06 20:29:51 -0500

    lesson 2

--- lesson2/lesson2.java
+++ lesson2/lesson2.java
@@ -1,4 +1,27 @@
 package lesson2;
+
+class MyClass implements Runnable {
+
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getPriority() + " Value: " + i );
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
 public class lesson2 {
+
+    public static void main(String[] arg) {
+        Thread t1 = new Thread(new MyClass());
+        Thread t2 = new Thread(new MyClass());
+        t1.start();
+        t2.start();
+    }
     
 }
