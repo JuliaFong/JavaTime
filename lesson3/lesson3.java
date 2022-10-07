f53955f3c3141bf3f8b245dbeb693a80a6b19949
Julia Fong <Juliamzfong@gmail.com>
2022-10-06 20:47:07 -0500

    lesson 3

---
+++ lesson3/lesson3.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+public class lesson3 {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread (new Runnable() {
+            public void run() {
+                for (int i =0 ; i <10; i++) {
+                    System.out.println(Thread.currentThread().getPriority() +  " Value : " + i );
+                }
+                try {
+                    Thread.sleep(100);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+    
+    
+        });
+    
+        t1.start();
+
+    }
+    
+}
